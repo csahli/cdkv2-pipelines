60140c459af82ef50397d4e45329c73124887646
Chaouki SAHLI <chaouki.sahli@gmail.com>
2021-12-07 10:08:33 -0500

    Initial commit

---
+++ test/cdkv2-pipeline.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Cdkv2Pipeline from '../lib/cdkv2-pipeline-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdkv2-pipeline-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Cdkv2Pipeline.Cdkv2PipelineStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
